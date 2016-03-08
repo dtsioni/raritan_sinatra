82e83d7509c6a633c934a806fedd63b667f38ae1
Daniel Tsioni <dtsioni@gmail.com>
2016-03-08 13:01:41 -0500

    Fixed some relations to account for new models

--- models/professor.rb
+++ models/professor.rb
@@ -1,6 +1,8 @@
 class Professor < ActiveRecord::Base
   belongs_to :department
   has_many :scores
+  has_many :aliases
+  has_and_belongs_to_many :tags
 
   validates_presence_of :first_name, :last_name
   validates_uniqueness_of :first_name, :scope => :last_name
