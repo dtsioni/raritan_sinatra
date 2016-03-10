d2093e02b4fe1d994ba1c615f2b0b91e400fc7df
Daniel Tsioni <dtsioni@gmail.com>
2016-03-10 11:23:13 -0500

    foo

--- app.rb
+++ app.rb
@@ -7,7 +7,7 @@ require './models/professor'
 require './models/score'
 # database config
 require './config/environments'
-
+set :database, {adapter: "postgresql", database: "raritan_development"}
 # our routes
 # post a new score for this professor
 post '/:school/:department/:professor' do
@@ -20,6 +20,12 @@ get '/:school/:department/professors' do
 end
 # return all departments from a school
 get '/:school/departments' do
+  content_type :json
+
+  School.find_by(name: params[:school]).departments.each do |foo|
+    puts foo.name
+  end
+  return "touch my grundle?"
 end
 # return all professors from a school
 get '/:school/professors' do
--- test.rb
+++ test.rb
@@ -1,23 +1,43 @@
 ENV['RACK_ENV'] = 'test'
+
 require 'minitest/autorun'
 require 'rack/test'
 require_relative 'app.rb'
 
 include Rack::Test::Methods
-
+set :database, {adapter: "postgresql", database: "raritan_test"}
 def app
   Sinatra::Application
 end
 
-describe "School" do
+before do
+  @school_params = { name: "Rutgers University - New Brunswick" }
+  @dept_cs_params = { name: "Computer Science" }
+  @dept_math_params = { name: "Math" }
+
+  school = School.create(@school_params)
+  dept1 = Department.create(@dept_cs_params)
+  dept2 = Department.create(@dept_math_params)
+  dept1.school = school
+  dept2.school = school
+  dept1.save
+  dept2.save
+  school.departments << dept1
+  school.departments << dept2
+  school.save
+end
+
+describe "Department" do
+
+
   it "should return json" do
-    get '/rutgers'
+    get '/Rutgers%20University%20-%20New%20Brunswick/departments'
     last_response.headers['Content-Type'].must_equal 'application/json'
   end
 
-  it "should return the correct school" do
-    get 'rutgers'
-    school_info = { school: "rutgers"}
-    last_response.body.must_equal school_info.to_json
+  it "should return a list of departments" do
+    get 'Rutgers%20University%20-%20New%20Brunswick/departments'
+    departments_info = { departments: ["Computer Science", "Math"] }
+    last_response.body.must_equal departments_info.to_json
   end
 end
