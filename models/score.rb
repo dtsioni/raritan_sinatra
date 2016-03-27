4cb042930420e84bed70603e5020600e51f7adfa
Daniel Tsioni <dtsioni@gmail.com>
2016-03-27 17:02:10 -0400

    Add validations to score

--- models/score.rb
+++ models/score.rb
@@ -2,4 +2,8 @@ class Score < ActiveRecord::Base
   belongs_to :professor
 
   validates_uniqueness_of :professor_id, :scope => :user_id
+
+  validates_presence_of :fairness, :helpfulness, :clarity
+  validates_inclusion_of :fairness, :helpfulness, :clarity, :in 1..5
+  validates_inclusion_of :preparation, :homework, :participation, :interesting, :attendance, :in 1..3
 end
