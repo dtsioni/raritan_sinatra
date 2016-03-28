9dc73ef536f788d002e2f422331f88a49f2ea139
Daniel Tsioni <dtsioni@gmail.com>
2016-03-28 10:44:27 -0400

    Formatting

--- models/score.rb
+++ models/score.rb
@@ -4,6 +4,6 @@ class Score < ActiveRecord::Base
   validates_uniqueness_of :professor_id, :scope => :user_id
 
   validates_presence_of :fairness, :helpfulness, :clarity
-  validates_inclusion_of :fairness, :helpfulness, :clarity, :in 1..5
-  validates_inclusion_of :preparation, :homework, :participation, :interesting, :attendance, :in 1..3
+  validates_inclusion_of :fairness, :helpfulness, :clarity, :in => 1..5
+  validates_inclusion_of :preparation, :homework, :participation, :interesting, :attendance, :in => 1..3
 end
