0f4ac2674999442779da0da6baf47ae3b764aa73
Daniel Tsioni <dtsioni@gmail.com>
2016-04-04 22:15:38 -0400

    Changed to be correct fields

--- db/migrate/20160405021208_change_score_decimals_to_floats.rb
+++ db/migrate/20160405021208_change_score_decimals_to_floats.rb
@@ -1,10 +1,11 @@
 class ChangeScoreDecimalsToFloats < ActiveRecord::Migration
   def change
-    change_column :scores, :fairness, :float
+    change_column :scores, :easiness, :float
     change_column :scores, :clarity, :float
     change_column :scores, :helpfulness, :float
-    change_column :scores, :preparation, :float
-    change_column :scores, :homework, :float
+    change_column :scores, :organization, :float
+    change_column :scores, :work, :float
     change_column :scores, :interesting, :float
+    change_column :scores, :pacing, :float
   end
 end
